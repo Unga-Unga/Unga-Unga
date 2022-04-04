e2262836bcef2a1fc7ef15746ed4f70a30fc9e3a
Jose Bravo <ja.bravo.isidro@gmail.com>
2022-04-04 20:58:12 +0200

    Styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import NextHead from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextHead>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <title>Unga Unga!</title>
+      </NextHead>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
